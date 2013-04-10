07f08bf193fd07aea3afcf46cd394686a7d52829
Nichada <nich_blue@hotmail.com>
2013-04-10 14:49:18 +0700

    add new iceport

--- Icetizen.java
+++ Icetizen.java
@@ -1,6 +1,7 @@
 import iceworld.given.IcetizenLook;
 import iceworld.given.MyIcetizen;
 
+<<<<<<< HEAD
 public class Icetizen implements MyIcetizen {
 	int IcePortID;
 	IcetizenLook look;
@@ -56,4 +57,68 @@ public class Icetizen implements MyIcetizen {
 		// TODO Auto-generated method stub
 		username2=e;
 	}
+=======
+
+public class Icetizen implements MyIcetizen {
+
+	int icePortID;
+	IcetizenLook look;
+	int listenPort;
+	String username;
+	
+	int port;
+	
+	int iceportID;
+	
+	@Override
+	public int getIcePortID() {
+		// TODO Auto-generated method stub
+		return icePortID;
+				
+	}
+
+	@Override
+	public IcetizenLook getIcetizenLook() {
+		// TODO Auto-generated method stub
+		return look;
+	}
+
+	@Override
+	public int getListeningPort() {
+		// TODO Auto-generated method stub
+		return port;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return username;
+	}
+
+	@Override
+	public void setIcePortID(int arg0) {
+		// TODO Auto-generated method stub
+		icePortID = arg0;
+		
+	}
+
+	@Override
+	public void setIcetizenLook(IcetizenLook arg0) {
+		look = arg0;
+		
+	}
+
+	@Override
+	public void setListeningPort(int arg0) {
+		// TODO Auto-generated method stub
+		listenPort=arg0;
+	}
+
+	@Override
+	public void setUsername(String arg0) {
+		// TODO Auto-generated method stub
+		username=arg0;
+	}
+
+>>>>>>> add new iceport
 }
